70e26150f2cb2f13aaa81d6b5c9a9bd25ed7b18e
[dti] Thiago Vieira Reginaldo <thiagovieiradti@gmail.com>
2019-06-01 04:41:59 -0300

    corerção de erro após auto-merging.

--- site/script/calculadora.js
+++ site/script/calculadora.js
@@ -4,7 +4,7 @@ export function Somar(operadorEsquerda, operadorDireita){
 };
 
 export function Somar3Numeros(operadorEsquerda, operadorMeio,operadorDireita){
-    Logar(`Somando ${operadorEsquerda}, ${operadorMeio}, ${operadorDireita}: `); 
+    LogarMensagem(`Somando ${operadorEsquerda}, ${operadorMeio}, ${operadorDireita}: `); 
     return operadorEsquerda + operadorMeio + operadorDireita;
 };
 
