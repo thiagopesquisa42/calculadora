ddabf425bb46c94ad19eee3c071158a8071c5256
[dti] Thiago Vieira Reginaldo <thiagovieiradti@gmail.com>
2019-06-01 04:33:08 -0300

    refactor: melhoria no cosolelog, adicionada função para abstrair o log.

--- site/script/calculadora.js
+++ site/script/calculadora.js
@@ -1,23 +1,27 @@
 export function Somar(operadorEsquerda, operadorDireita){
-    console.log(`Somando ${operadorEsquerda} + ${operadorDireita}: `); 
+    Logar(`Somando ${operadorEsquerda} + ${operadorDireita}: `); 
     return operadorEsquerda + operadorDireita;
 };
 
 export function Dividir(dividendo, divisor){
     if(divisor == 0){
-        console.log(`Erro, divisão por zero`); 
+        Logar(`Erro, divisão por zero`); 
         return;
     }
-    console.log(`Dividindo ${dividendo} / ${divisor}: `); 
+    Logar(`Dividindo ${dividendo} / ${divisor}: `); 
     return dividendo / divisor;
 };
 
 export function Subtrair(operadorEsquerda, operadorDireita){
-    console.log(`Subtrair ${operadorEsquerda} - ${operadorDireita}: `); 
+    Logar(`Subtrair ${operadorEsquerda} - ${operadorDireita}: `); 
     return operadorEsquerda - operadorDireita;
 };
 
 export function Multiplicar(operadorEsquerda, operadorDireita){
-    console.log(`Multiplicando ${operadorEsquerda} X ${operadorDireita}: `); 
+    Logar(`Multiplicando ${operadorEsquerda} X ${operadorDireita}: `); 
     return operadorEsquerda * operadorDireita;
 };
+
+function Logar(mensagem){
+    console.log(mensagem);
+}
